07501ee236cb38ad145424b33bde02c442a40917
tjip1234 <rafaeldiederen@gmail.com>
2023-09-06 15:03:04 +0200

    main

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-
-document.querySelectorAll('.blog-link').forEach(link => {
-    link.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        const articleUrl = this.getAttribute('href');  
-        window.open(articleUrl, '_blank');
-
-    });
-});
-
-window.onload = function() {
-        const inIframe = window !== window.top;
-        console.log(inIframe)
-        if (inIframe) {
-            document.querySelectorAll('.blog-link').forEach(link => {
-                const fullArticle = link.parentNode.nextElementSibling;
-                fullArticle.style.display = 'none';
-            });
-        }
-        if (!inIframe){
-            document.querySelectorAll('.blog-link').forEach(link => {
-                link.remove()
-            }); 
-        }
-};
-
-     
-
-
 document.addEventListener('DOMContentLoaded', function() {
 
     let tagButtons = document.querySelectorAll('.tag-button');
@@ -82,5 +52,61 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+function applyDarkModeToIframes() {
+    const iframes = document.querySelectorAll('iframe.iframe-thing');
+
+    iframes.forEach(iframeElement => {
+        iframeElement.onload = () => {
+            try {
+                const iframeContent = iframeElement.contentDocument || iframeElement.contentWindow.document;
+                const body = iframeContent.body;
+
+                if (localStorage.getItem('theme') === 'dark') {
+                    body.classList.add('dark-mode');
+                } else {
+                    body.classList.remove('dark-mode');
+                }
+            } catch (error) {
+                console.error("Failed to apply dark mode to an iframe:", error);
+            }
+        };
+
+        // Force reload the iframe content to clear any cached versions
+        iframeElement.src = iframeElement.src;
+    });
+}
+
+function toggleDarkMode() {
+    const bodyDocu = document.body;
+    const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme === 'dark') {
+        bodyDocu.classList.remove('dark-mode');
+        localStorage.setItem('theme', 'light');
+    } else {
+        bodyDocu.classList.add('dark-mode');
+        localStorage.setItem('theme', 'dark');
+    }
+
+    applyDarkModeToIframes(currentTheme); // This ensures dark mode is applied to iframes when toggling
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const bodyDocu = document.body;
+    const currentTheme = localStorage.getItem('theme');
+
+    // Apply dark mode to the main document
+    if (currentTheme === 'dark') {
+        bodyDocu.classList.add('dark-mode');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        bodyDocu.classList.remove('dark-mode');
+        localStorage.setItem('theme', 'light');
+    }
+
+    applyDarkModeToIframes(currentTheme); // Apply dark mode to iframes once the document loads
+});
+
 
 
